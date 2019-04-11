9d74e7c11810b31cbfd18ace710c287942a1c22f
satyazmx <thakursatya6@gmail.com>
2019-04-11 22:55:12 +0530

    cusotmer Info complete

--- Task/src/app/app-routing.module.ts
+++ Task/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { PaymentComponent } from './payment/payment.component';
 
-const appRoutes : Routes=[];
+const appRoutes : Routes=[
+    { path :'customer', component: CustomerComponent},
+    { path :'shipping', component: ShippingComponent},
+    { path :'payment', component: PaymentComponent},
+];
 
 @NgModule({
     imports: [ RouterModule.forRoot(appRoutes)],
--- Task/src/app/app.module.ts
+++ Task/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule} from './app-routing.module'
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { PaymentComponent } from './payment/payment.component';
+
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    ShippingComponent,
+    PaymentComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,AppRoutingModule
